dc69aab30726d5647667bd749c56f0f1a758f1e0
Ubuntu <ubuntu@ip-172-31-22-178.ec2.internal>
2015-09-17 16:00:33 +0000

    update

--- R/ConvertFormat.R
+++ R/ConvertFormat.R
@@ -3,12 +3,12 @@ ConvertPdf2Png<-function(fn.pdf, resize=80, density=150) {
   # resize  0-100; output image size
   # density image pixel density; value more than 600 will generate very large file
   
-  if (grepl('.pdf$', fn.pdf, ignore.case = TRUE)) fn.png<-sub('.pdf$', '.png', fn, ignore.case = TRUE) else fn.png<-paste(fn.pdf, '.png', sep='');
+  if (grepl('.pdf$', fn.pdf, ignore.case = TRUE)) fn.png<-sub('.pdf$', '.png', fn.pdf, ignore.case = TRUE) else fn.png<-paste(fn.pdf, '.png', sep='');
   
   if (!grepl('%$', resize)) resize<-paste(resize, '%', sep='');
   cmmd<-paste('convert -density', density, '-resize', resize, fn.pdf, fn.png);
   
-  system(cmmd);
-  print(cmmd);
+  x<-system(cmmd);
+
   fn.png;
 }
