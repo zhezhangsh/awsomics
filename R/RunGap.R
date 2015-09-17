154ac4a7028d69492ed206c68e9f9b91da4ef76e
Ubuntu <ubuntu@ip-172-31-22-178.ec2.internal>
2015-09-17 20:17:48 +0000

    update

---
+++ R/RunGap.R
@@ -0,0 +1,26 @@
+# Make multiple run of gap analysis to find the optimal number of k for kmean cluster
+
+RunGap<-function(d, k.max=ncol(d)-1, n.run=20, n.bootstrap=100, n.node=n.run) {
+  # d   The data matrix, assume each column is a sample
+  
+  d<-t(d);
+  
+  # Gap core function, return gap statistics
+
+  if (n.node > 1) { # run in parallel
+    runG<-function(d, k, n) cluster::clusGap(d, kmeans, k, n)[[1]][, 3];
+    ds<-lapply(1:n.run, function(i) d); 
+    cl<-snow::makeCluster(n.node, type='SOCK');
+    gap<-snow::clusterApplyLB(cl, ds, runG, k=k.max, n=n.bootstrap);
+    snow::stopCluster(cl);
+  } else {
+    gap<-lapply(1:n.run, function(i) cluster::clusGap(d, kmeans, k.max, n.bootstrap)[[1]][, 3]);
+  }
+  
+  # all gap scores
+  gap<-do.call('cbind', gap);
+  rownames(gap)<-1:k.max;
+  colnames(gap)<-paste('run', 1:n.run);
+  
+  gap;
+}
--- load_by_url.r
+++ load_by_url.r
@@ -21,6 +21,7 @@ source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangaws
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/PlotPCA.R");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/PlotVenn.R");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/ProcessGADEM.R");
+source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/RunGap.R");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/SearchGwasDb.R");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/SearchVariantDb.R");
 source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master//zhangawsomics/R/TestGSE.R");
