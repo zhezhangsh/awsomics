338983082f40b098abd833870d13f53a0956d8e9
root <root@ip-172-31-22-178.ec2.internal>
2015-09-18 21:06:15 +0000

    gap

--- R/RunGap.R
+++ R/RunGap.R
@@ -8,13 +8,13 @@ RunGap<-function(d, k.max=ncol(d)-1, n.run=20, n.bootstrap=100, n.node=n.run) {
   # Gap core function, return gap statistics
 
   if (n.node > 1) { # run in parallel
-    runG<-function(d, k, n) cluster::clusGap(d, kmeans, k, n)[[1]][, 3];
+    runG<-function(d, k, n) cluster::clusGap(d, kmeans, k, n)[[1]];
     ds<-lapply(1:n.run, function(i) d); 
     cl<-snow::makeCluster(n.node, type='SOCK');
     gap<-snow::clusterApplyLB(cl, ds, runG, k=k.max, n=n.bootstrap);
     snow::stopCluster(cl);
   } else {
-    gap<-lapply(1:n.run, function(i) cluster::clusGap(d, kmeans, k.max, n.bootstrap)[[1]][, 3]);
+    gap<-lapply(1:n.run, function(i) cluster::clusGap(d, kmeans, k.max, n.bootstrap)[[1]]);
   }
   
   # all gap scores
