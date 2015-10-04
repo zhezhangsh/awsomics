34285bcb3cf0b92382f01f1456d2798928789b3b
zhezhangsh <zhangz@email.chop.edu>
2015-10-04 01:12:29 -0400

    format num

---
+++ R/FormatNumeric.R
@@ -0,0 +1,19 @@
+# format numeric columns in a matrix of data.frame
+FormatNumeric<-function(tbl, digit=4) {
+  for (i in 1:ncol(tbl)) {
+    x<-tbl[, i];
+    if (is.numeric(x)) {
+      x<-as.vector(x); 
+      if (!identical(round(x), x)) {
+        if (max(abs(x))>1) x<-round(x, digit) else {
+          y<-sign(x);
+          a<-floor(log10(abs(x)));
+          z<-round(abs(x)/10^a, 1);
+          x<-y*z*10^a;
+        }
+      }
+    tbl[, i]<-x;
+    }
+  }
+  tbl;
+}
