ad2461f20b7c06fc999708bd0aa1aa886be06eef
root <root@ip-172-31-22-178.ec2.internal>
2016-02-03 20:34:48 +0000

    plot colored blocks

---
+++ R/PlotColoredBlocks.R
@@ -0,0 +1,143 @@
+
+# Default method to plot customized heatmap-like blue-red colored grid, with column and row name labels
+PlotColoredBlock<-function(d, min=NA, max=NA, num.breaks=15, key='') {
+  # d             Data matrix to plot; column and row names will be plotted
+  # num.breaks    Number of color breakpoints
+  # min, max      Minimal and maximal values to plot; round values lower or higher than the values
+  # key           Plot key if not empty
+  
+  library(awsomics); 
+  
+  if (num.breaks<1) num.breaks<-32;
+  col<-GetBluePickogramColors(num.breaks);
+  
+  if (is.na(min)) min<-min(d, na.rm=TRUE); 
+  if (is.na(max)) max<-max(d, na.rm=TRUE); 
+  
+  if (min > min(d, na.rm=TRUE)) d[!is.na(d) & d<min]<-min;
+  if (max < max(d, na.rm=TRUE)) d[!is.na(d) & d>max]<-max;
+  breaks<-seq(min, max, length.out = length(col)+1); 
+  
+  # column/row names
+  rnm<-rownames(d); 
+  cnm<-colnames(d); 
+  
+  nr<-nrow(d);
+  nc<-ncol(d);
+  
+  # Current device size in inch
+  W<-par()$fin[1] - .2;
+  H<-par()$fin[2] - .2; 
+  str.h<-strheight('', unit='inches'); # default string height
+  
+  # Total block size
+  block.w<-1.2*str.h*nc; 
+  block.h<-1.2*str.h*nr;  
+  
+  ratio<-c(0.1, 10); # minimal and maximal label/block ratio
+  
+  # lable size
+  wid.r<-max(strwidth(rnm, units='inches')); 
+  wid.c<-max(strwidth(cnm, units='inches')); 
+  
+  str.r<-wid.r/max(1, str.h/(0.75*H/nr)); 
+  str.c<-wid.c/max(1, str.h/(0.75*W/nc));
+  
+  str.r<-max(ratio[1]*block.w, min(ratio[2]*block.w, str.r)); 
+  str.c<-max(ratio[1]*block.h, min(ratio[2]*block.h, str.c)); 
+  
+  # label + block size
+  full.w<-block.w+str.r;
+  full.h<-block.h+str.c;
+  
+  # ratio to device size
+  ratio.w<-W/full.w;
+  ratio.h<-H/full.h;
+  
+  # plot heatmap
+  par(mar=c(0.1, 0.1, 0.1, 0.1)); 
+  limit.w<-ceiling(full.w/(block.w/nc)); 
+  limit.h<-ceiling(full.h/(block.h/nr)); 
+  plot(0, type='n', xlim=c(0, limit.w), ylim=c(0, limit.h), xaxs='i', yaxs='i', axes=FALSE, xlab='', ylab='');
+  image(0:nc, 0:nr, t(d[nr:1,]), col=col, breaks=breaks, add=TRUE);
+  
+  # plot row names
+  space.w<-(1-nc/limit.w)*W;
+  cex.w<-(space.w-0.2)/wid.r; 
+  cex.w<-min(cex.w, 0.66*(H/limit.h)/str.h); 
+  text(nc, (nr:1)-0.5, pos=4, label=rnm, cex=cex.w);
+  
+  # plot column names
+  space.h<-(1-nr/limit.h)*H;
+  cex.h<-(space.h-0.2)/wid.c; 
+  cex.h<-min(cex.h, 0.66*(W/limit.w)/str.h); 
+  text((1:nc)-0.5, (nr+limit.h)/2, srt=90, label=cnm, cex=cex.h);
+  
+  # Plot key
+  if (!is.na(key) & key!='') {
+    # key name
+    key.w<-0.6*(space.w-0.2)/strwidth(key, units='inches');
+    key.h<-0.2*space.h/str.h
+    text((nc+limit.w)/2, nr, pos=3, label=key, col='purple', cex=min(key.w, key.h)); 
+    
+    key.w<-limit.w-nc; 
+    key.h<-limit.h-nr;
+    
+    key.x<-c(nc + 0.15*key.w, nc + 0.85*key.w); 
+    key.y<-c(nr + 0.4*key.h, nr + 0.4*key.h + min(nr/25, 0.4*key.h)); 
+    image(seq(key.x[1], key.x[2], length.out=num.breaks+1), key.y, matrix(breaks, nc=1), col=col, breaks=breaks, add=TRUE);
+    
+    min<-round(min, ceiling(log10(abs(min)))); 
+    max<-round(max, ceiling(log10(abs(min)))); 
+    
+    text(nc+0.15*key.w, nr + 0.4*key.h, pos=1, label=min, cex=0.8*cex.h); 
+    text(nc+0.85*key.w, nr + 0.4*key.h, pos=1, label=max, cex=0.8*cex.h); 
+  }
+  
+  abline(v=0:nc, h=0:nr, lwd=0.25, col='#333333');
+  box();
+}
+
+# Return blue-pinkogram colors. 
+GetBluePickogramColors<-function(n) {
+  # n   Number of colors
+  
+  library(gplots);
+  
+  if (n<2) {
+    warning('Number of colors less than 2, return NA');
+    NA;
+  } else {
+    l<-floor(n/2); 
+    
+    if (l<=4) lo<-c('#A9A9FF', '#FF9DB0') else 
+      if (l<=8) lo<-c('#D5D5FF', '#FFAADA') else
+        if (l<=16) lo<-c('#E2DFFF', '#FFC2E6') else
+          lo<-c('#EEE5FF', '#FFE5EE');
+    
+    col1<-colorpanel(l, '#0000FF', lo[1]);
+    col2<-colorpanel(l, lo[2], '#FF0000');
+    
+    if (2*l==n) c(col1, col2) else c(col1, '#EEE5EE', col2); 
+  }
+}
+
+# Helper function of PlotColoredBlock(), calculate preferred device size for color block heatmap
+CalculateColoredBlockSize<-function(d, ratio=1, max.size=12) {
+  # d         The matix to be plotted
+  # ratio     Default height-to-width ratio of each block
+  # max.size  Maximum width and height allowed for the whole plot, in inches
+  
+  sz<-0.2;
+  
+  nr<-nrow(d);
+  nc<-ncol(d);
+  
+  block.w<-sz*nc;
+  block.h<-ratio*sz*nr;
+  
+  full.w<-block.w + ratio*0.12*max(nchar(rownames(d))) + 0.4; 
+  full.h<-block.h + 0.12*max(nchar(colnames(d))) + 0.4; 
+  
+  c(full.w, full.h)/max(1, max(c(full.w, full.h))/max.size); 
+}
