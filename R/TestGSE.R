bef0c2b927edad1f3d7aa6e9253112e22e61e762
zhezhangsh <zhangz@email.chop.edu>
2016-02-13 00:39:46 -0500

    GSE test

--- R/TestGSE.R
+++ R/TestGSE.R
@@ -56,36 +56,68 @@ TestGSE<-function(gs, u, coll, size.min=10, size.max=500, p.cutoff=0.05) {
 
 
 # Summarize, format, and write out results of gene set enrichment analysis from the TestGSE function
-WrapGSE<-function(stat, anno, output='.', prefix='GSE') {
-  # stat    Statistic table, the first element of the TestGSE outputs
-  # anno    Gene set annotation
-  # output  Path to output files
-  # prefix  prefix to output file names
+WrapGSE<-function(stat, anno, split.collection=FALSE, output=getwd()) {
+  # stat              Statistic table, the first element of the TestGSE outputs
+  # anno              Gene set annotation
+  # output            Path to output files
+  # split.collection  Whether to further split gene sets by their collection, default is FALSE
   
   if (!file.exists(output)) dir.create(output, recursive = TRUE);
   
   library(awsomics);
   
-  src<-sort(unique(anno[[1]])); 
   stat<-stat[rownames(stat) %in% rownames(anno), , drop=FALSE]; 
   stat<-awsomics::FormatNumeric(stat); 
   anno<-anno[rownames(stat), ]; 
+  anno$Collection<-sub('/', '-', anno$Collection); 
   
   # prepare full table
   tbl1<-tbl0<-cbind(anno, stat); 
   tbl1$Name<-AddHref(tbl1$Name, tbl1$URL);
   tbl1<-tbl1[, !(colnames(tbl1) %in% c('URL', 'N_Set', 'FWER'))];
   
-  # split by source
-  tbls<-lapply(src, function(x) tbl1[tbl1$Source==x, -1, drop=FALSE]); 
-  names(tbls)<-src; 
-  
-  # write datatables
-  fn<-sapply(names(tbls), function(nm) {
-    fn<-paste(output, paste(prefix, '_', nm, '.html', sep=''), sep='/'); 
-    CreateDatatable(tbls[[nm]], fn, caption = nm); 
-  }); 
-  
+  if (!split.source) {
+    tbls<-tbl1;
+    fn<-CreateDatatable(tbls, paste(output, 'GSE_All_Sig.html', sep='/'));
+  } else {
+    src<-sort(unique(anno[[1]])); 
+    
+    # split by source
+    tbls<-lapply(src, function(x) tbl1[tbl1$Source==x, -1, drop=FALSE]); 
+    names(tbls)<-src;
+    
+    ##########
+    if (split.collection) {
+      cll<-lapply(split(anno$Collection, anno$Source), unique)[src];
+      cll<-lapply(cll, sort); 
+      tbls<-lapply(src, function(s) lapply(cll[[s]], function(c) tbls[[s]][tbls[[s]]$Collection==c, -1, drop=FALSE])); 
+      names(tbls)<-src;
+      for (i in 1:length(tbls)) names(tbls[[i]])<-cll[[i]];
+      
+      # write datatables
+      fn<-lapply(names(tbls), function(nm) { 
+        pth<-paste(output, nm, sep='/'); 
+        if (!file.exists(pth)) dir.create(pth, recursive = TRUE); 
+        t<-tbls[[nm]]; 
+        f<-sapply(names(t), function(nm2) {
+          f1<-paste(pth, paste(nm2, '.html', sep=''), sep='/');
+          f2<-CreateDatatable(t[[nm2]], f1, caption = paste(nm, nm2, sep=': ')); 
+          f1;
+        }); 
+        names(f)<-names(t); 
+        f;
+      }); 
+      names(fn)<-src;
+    } else {
+      # write datatables
+      fn<-sapply(names(tbls), function(nm) {
+        fn<-paste(output, paste(nm, '.html', sep=''), sep='/'); 
+        CreateDatatable(tbls[[nm]], fn, caption = nm); 
+      }); 
+      names(fn)<-src;
+    }
+  }
+
   saveRDS(tbl0, file=paste(output, paste(prefix, '_All_Sig.rds', sep=''), sep='/'));
   
   list(all=tbl0, formatted=tbls, file=fn); 
