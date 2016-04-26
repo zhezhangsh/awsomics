8b2bf8beeda84d9171c722376a6a4e7d1f764dc9
zhezhangsh <zhangz@email.chop.edu>
2016-04-26 11:49:24 -0400

    file path

--- R/ManipulateFilePath.R
+++ R/ManipulateFilePath.R
@@ -19,4 +19,17 @@ TrimPath<-function(path) {
   sapply(strsplit(path, '/'), function(x) x[length(x)]); 
 }
 
-
+# Remove file extension
+RemoveExtension<-function(path) {
+  path<-sapply(path, function(f) {
+    is.dir<-file.info(f)$isdir; 
+    if (!is.dir | is.na(is.dir)) {
+      x<-strsplit(f, '/')[[1]]; 
+      mx<-max(gregexpr('\\.', x[length(x)])[[1]]);
+      if (mx <= 1) mx<-nchar(x[length(x)]) else mx<-mx-1; 
+      x[length(x)]<-substr(x[length(x)], 1, mx); 
+      paste(x, collapse='/'); 
+    } else f;
+  }); 
+  path;
+}
