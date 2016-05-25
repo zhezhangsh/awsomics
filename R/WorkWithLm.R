24606fbdf940cca330de10003dd1f2723275052f
Zhang <zhangz@email.chop.edu>
2016-05-25 10:32:23 -0400

    lm sigma

---
+++ R/WorkWithLm.R
@@ -0,0 +1,35 @@
+### Helper function related to linear models
+
+# Calculate number of sigma for each observation, using linear model built with all other observations
+CalcLmSigma <- function(x, y, plot=FALSE, xlab=NA, ylab=NA) {
+  # x, y        Vectors to fit linear model lm(y~x). Must have the same length
+  # plot        Whether to make the regression plot
+  # xlab, ylab  Axis labels for the plot
+  
+  c <- cbind(x, y); 
+  
+  sigma <- sapply(1:nrow(c), function(i) {
+    x<-c[-i, ];
+    mdl<-lm(x[, 2]~x[, 1]); 
+    coe<-as.vector(coefficients(mdl)); 
+    prd<-c[i, 1]*coe[2]+coe[1]; 
+    sig<-summary(mdl)$sigma;
+    (prd-c[i, 2])/sig; 
+  }); 
+  
+  if (plot) {
+    par(mar=c(5,5,2,2));
+    col <- rep('#88888888', length(x)); 
+    col[abs(sigma) >= 1.5] <- '#FF888888'; 
+    col[abs(sigma) >= 3.0] <- '#FF000088'; 
+    plot(x, y, col=col, pch=19, cex=2, xlab='Total input (million)', ylab='Uniquely mapped (million)', cex.lab=1.5); 
+    points(x, y, col='black', pch=19, cex=.2);
+    abline(lm(y~x), col='blue', lty=2);
+    legend('topleft', legend=c('Num_Sigma < 1.5', 'Num_Sigma >= 1.5', 'Num_Sigma >=3.0'), cex=1.25, bty='n', pch=18, 
+           col=c('#88888888', '#FF888888', '#FF000088')); 
+    abline(lm(y~x), col='blue', lty=2);
+  }
+  
+  names(sigma) <- names(y); 
+  sigma;
+}
